d42259597538f323869655421e2eebec061c9f6c
Ana Betancourt <anaa.betancourt13@gmail.com>
2020-10-08 06:37:18 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,14 +1,22 @@
 module Memorable
+  module ClassMethods
+    
+    def reset_all
+      all.clear
+    end 
+    
+    def count
+      all.count
+    end
   
-  def reset_all
-    all.clear
-  end 
-  
-  def count
-    all.count
   end
   
-  
+  module InstanceMethods
+    
+    def initialize
+      self.class.all << self 
+    end
+  end
   
   
   
